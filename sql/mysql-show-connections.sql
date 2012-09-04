d26774b547b341e168f202081dabc92684cfbfb7
mt <mathew.thomas@gmail.com>
2012-09-04 16:44:27 -0400

    Display more info from process table

--- sql/mysql-show-connections.sql
+++ sql/mysql-show-connections.sql
@@ -1,2 +1,3 @@
-SELECT db, time, CONCAT(user,'@',host) AS connection, command 
+SELECT db, time, CONCAT(user,'@',host) AS connection
+     , CONCAT(command, ': ', info) as command 
   FROM information_schema.processlist;
