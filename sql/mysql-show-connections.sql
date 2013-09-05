7b3be1c6568607c8f9630a48f8bc5ad5ddf200b5
mt <mathew.thomas@gmail.com>
2013-09-05 17:52:32 -0400

    Adding query for old connections

--- sql/mysql-show-connections.sql
+++ sql/mysql-show-connections.sql
@@ -1,3 +1,10 @@
-SELECT db, time, CONCAT(user,'@',host) AS connection
+SELECT id, db, time, CONCAT(user,'@',host) AS connection
      , CONCAT(command, ': ', info) as command 
   FROM information_schema.processlist;
+
+
+-- Connections older than 10 minutes
+SELECT id, db, time/60 as time, CONCAT(user,'@',host) AS connection
+     , CONCAT(command, ': ', info) as command 
+  FROM information_schema.processlist
+WHERE time/60 > 10;
